41fdac82978eb41647ad924363f7f412850fe709
Mjulianfr001056 <222112217@stis.ac.id>
2024-10-08 13:09:06 +0700

    final submission commit

--- app/src/main/java/org/bangkit/dicodingevent/data/retrofit/ApiService.kt
+++ app/src/main/java/org/bangkit/dicodingevent/data/retrofit/ApiService.kt
@@ -1,10 +1,8 @@
 package org.bangkit.dicodingevent.data.retrofit
 
-import org.bangkit.dicodingevent.data.response.DetailDicodingEventResponse
 import org.bangkit.dicodingevent.data.response.DicodingEventResponse
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
