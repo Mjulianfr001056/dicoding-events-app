bdbb8b7718219003dc364413cee93d892fc34215
Mjulianfr001056 <222112217@stis.ac.id>
2024-10-08 09:53:04 +0700

    create search mechanism for finished events

--- app/src/main/java/org/bangkit/dicodingevent/ui/ui/finished/FinishedFragment.kt
+++ app/src/main/java/org/bangkit/dicodingevent/ui/ui/finished/FinishedFragment.kt
@@ -2,16 +2,17 @@ package org.bangkit.dicodingevent.ui.ui.finished
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.bangkit.dicodingevent.R
 import org.bangkit.dicodingevent.data.response.DicodingEvent
 import org.bangkit.dicodingevent.databinding.FragmentFinishedBinding
-import org.bangkit.dicodingevent.databinding.FragmentUpcomingBinding
 import org.bangkit.dicodingevent.ui.DetailActivity
 import org.bangkit.dicodingevent.ui.DicodingEventAdapter
 
@@ -43,8 +44,51 @@ class FinishedFragment : Fragment() {
             setEventList(events, adapter)
         }
 
+        binding.rvSearchResults.layoutManager = LinearLayoutManager(requireActivity())
+        val searchAdapter = DicodingEventAdapter { dicodingEvent ->
+            val intent = Intent(requireActivity(), DetailActivity::class.java)
+            intent.putExtra(DetailActivity.EXTRA_EVENT, dicodingEvent)
+            startActivity(intent)
+        }
+        binding.rvSearchResults.adapter = searchAdapter
+
+        viewModel.searchResults.observe(viewLifecycleOwner) { filteredEvents ->
+//            Log.d(TAG, "onViewCreated: $filteredEvents")
+            if (!filteredEvents.isNullOrEmpty()) {
+                binding.rvSearchResults.visibility = View.VISIBLE
+                binding.rvEvent.visibility = View.GONE
+                searchAdapter.submitList(filteredEvents)
+            } else {
+                binding.rvSearchResults.visibility = View.GONE
+                binding.rvEvent.visibility = View.VISIBLE
+            }
+        }
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                .setOnEditorActionListener { textView, _, _ ->
+                    searchBar.setText(searchView.text)
+                    searchView.hide()
+                    viewModel.searchEvent(textView.text.toString())
+                    false
+                }
+        }
+
         viewModel.isLoading.observe(viewLifecycleOwner) { isLoading ->
             showLoading(isLoading, binding)
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            binding.searchBar.clearText()
+
+            if (binding.rvSearchResults.visibility == View.VISIBLE) {
+                binding.rvSearchResults.visibility = View.GONE
+                binding.rvEvent.visibility = View.VISIBLE
+            } else {
+                findNavController().popBackStack()
+            }
         }
     }
 
--- app/src/main/java/org/bangkit/dicodingevent/ui/ui/finished/FinishedViewModel.kt
+++ app/src/main/java/org/bangkit/dicodingevent/ui/ui/finished/FinishedViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import org.bangkit.dicodingevent.data.response.DicodingEventResponse
 import org.bangkit.dicodingevent.data.response.ListEventsItem
 import org.bangkit.dicodingevent.data.retrofit.NetworkModule
+import org.bangkit.dicodingevent.ui.ui.upcoming.UpcomingViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,6 +15,9 @@ import retrofit2.Response
 class FinishedViewModel : ViewModel() {
     private val _eventList = MutableLiveData<List<ListEventsItem>>()
     val eventlist : LiveData<List<ListEventsItem>> = _eventList
+
+    private val _searchResults = MutableLiveData<List<ListEventsItem>>()
+    val searchResults: LiveData<List<ListEventsItem>> = _searchResults
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
@@ -39,6 +43,36 @@ class FinishedViewModel : ViewModel() {
                     val responseBody = response.body()
                     if (responseBody != null) {
                         _eventList.value = response.body()?.listEvents
+                    }
+                }else {
+                    Log.d(TAG, "onResponse: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<DicodingEventResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
+    }
+
+    fun searchEvent(
+        query: String
+    ){
+        _isLoading.value = true
+        val client = NetworkModule.getApiService().searchEvents(-1, query)
+
+        client.enqueue(object : Callback<DicodingEventResponse> {
+            override fun onResponse(
+                call: Call<DicodingEventResponse>,
+                response: Response<DicodingEventResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    val responseBody = response.body()
+                    if (responseBody != null) {
+                        _searchResults.value = response.body()?.listEvents
+//                        Log.d(TAG, "onResponse: ${_searchResults.value}")
                     }
                 }else {
                     Log.d(TAG, "onResponse: ${response.message()}")
